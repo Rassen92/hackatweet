6a4fdd5cea5a77d827345ed91f3b7a7f7915b5c2
Ghassan <rassen92@gmail.com>
2022-10-06 12:44:52 +0200

    reducer en plus

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore, persistReducer } from 'redux-persist';
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage';
+
 
 function App({ Component, pageProps }) {
   return (
