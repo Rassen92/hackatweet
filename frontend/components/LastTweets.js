dcce0a0aa1e5e4d8551ec3242472c45107e93c87
Gaël Moigne <gmoigne@live.fr>
2022-10-06 17:06:58 +0200

    user tweet et lasttweet ok

--- frontend/components/LastTweets.js
+++ frontend/components/LastTweets.js
@@ -1,5 +1,15 @@
+import Card from "./Card";
+import styles from "../styles/LastTweets.module.css";
+
 function LastTweets() {
-  return <div>LastTweets</div>;
+  return (
+    <div className={styles.container}>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
 }
 
 export default LastTweets;
--- frontend/components/User.js
+++ frontend/components/User.js
@@ -3,19 +3,9 @@ import styles from "../styles/User.module.css";
 function User() {
   return (
     <div className={styles.container}>
-      <img
-        className={styles.logoImg}
-        src="logo.png"
-        width="50px"
-        height="50px"
-      />
+      <img className={styles.logoImg} src="logo.png" />
       <div className={styles.user}>
-        <img
-          className={styles.profileImg}
-          src="profile.png"
-          width="50px"
-          height="50px"
-        />
+        <img className={styles.profileImg} src="profile.png" />
         <div className={styles.userText}>
           <span>FirstName</span>
           <span>@UserName</span>
